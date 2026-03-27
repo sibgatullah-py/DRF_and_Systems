cc39657de42fe25e8503bffa11aa7a3fb77c0999
sibgatullah-dev <sibgatullah881@gmail.com>
2026-03-27 08:27:44 +0600

    serializar creation done , prociding to views

---
+++ Employee_Management/EmployeeManagement/api/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from .models import *
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+        extra_kwargs = {
+            "password":{"write_only":True}
+        }
+        
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password) # Hashes the password instead of plain text
+        user.save()
+        return user
+    
+    
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = "__all__"
+        
+
+class AttendanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attendance
+        fields = "__all__"
+        
+        
+class WorkLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WorkLog
+        fields = "__all__"
