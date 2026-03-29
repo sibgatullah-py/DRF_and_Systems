0c4df3b576bf68b4898227c0c60a568e92c0143e
sibgatullah-dev <sibgatullah881@gmail.com>
2026-03-29 07:37:32 +0600

    urls completed

--- Employee_Management/EmployeeManagement/api/urls.py
+++ Employee_Management/EmployeeManagement/api/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    
+    path("sign-in/", SignInView.as_view()),
+    path("sign-out/", SignOutView.as_view()),
+
+    path("my-attendance/", MyAttendanceView.as_view()),
+    path("my-worklogs/", MyWorkLogView.as_view()),
+    path("create-worklog/", WorkLogCreateView.as_view()),
+
+    path("team-members/", TeamMembersView.as_view()),
+    path("team-attendance/", TeamAttendanceView.as_view()),
+    path("team-worklogs/", TeamWorkLogView.as_view()),
 ]
