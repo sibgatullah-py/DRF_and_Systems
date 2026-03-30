8c323ec2c95af417e7bf87e379f3b2d71732cd11
sibgatullah-dev <sibgatullah881@gmail.com>
2026-03-30 21:48:36 +0600

    testing api

--- Employee_Management/EmployeeManagement/EmployeeManagement/urls.py
+++ Employee_Management/EmployeeManagement/EmployeeManagement/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('app/', include('api.urls')),
+    
+    # This is a login token generator
+    path('api/token/', TokenObtainPairView.as_view()), # method of simple JWT 
+    path('api/token/refresh/', TokenRefreshView.as_view()), # method of simple JWT
 ]
